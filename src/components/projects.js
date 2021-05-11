4d972bfe5500ad8786f4f566edc3b66bcc9bab4c
varunsh17 <81704366+varunsh17@users.noreply.github.com>
2021-05-12 00:35:44 +0530

    updated

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,8 +4,19 @@ function projects() {
     return (
         <div className='parallax4' id='projects'>
             <div className='content4'>
-            MY PROJECTS</div>
+            MY PROJECT 1</div>
+            <div className='content4'>
+            MY PROJECT 2</div>
+            <div className='content4'>
+            MY PROJECT 3</div>
+            <div className='content4'>
+            MY PROJECT 4</div>
+            <div className='content4'>
+            MY PROJECT 5</div>
+            
         </div>
+
+
     )
 }
 
